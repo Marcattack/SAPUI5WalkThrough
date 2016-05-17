9087a7fecca08492beb4dc7abcf64bdf09c2eaec
Marcattack <marcuhart@gmail.com>
2016-05-17 16:41:12 +0200

    Step 16 : Dialogs and Fragments

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -14,7 +14,22 @@ sap.ui.define([
 
 			// show message
 			MessageToast.show(sMsg);
+		},
+		
+      	_getDialog : function () {
+			if (!this._oDialog) {
+				this._oDialog = sap.ui.xmlfragment("sap.ui.demo.wt.view.HelloDialog");
+			this.getView().addDependent(this._oDialog);
+			}
+			return this._oDialog;
+      	},
+		  
+      	onOpenDialog : function () {
+        	this._getDialog().open();
+      	},
+		  
+		onCloseDialog : function () {
+			this._getDialog().close();
 		}
 	});
-
 });

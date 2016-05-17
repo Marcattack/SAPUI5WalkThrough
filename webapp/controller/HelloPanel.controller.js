aaddedb5bfa365cd6db171eddb6303d14a8505fe
Marcattack <marcuhart@gmail.com>
2016-05-17 17:41:34 +0200

    Step 19 : Reuse Dialogs

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -15,21 +15,8 @@ sap.ui.define([
 			// show message
 			MessageToast.show(sMsg);
 		},
-		
-      	_getDialog : function () {
-			if (!this._oDialog) {
-				this._oDialog = sap.ui.xmlfragment("sap.ui.demo.wt.view.HelloDialog", this);
-			this.getView().addDependent(this._oDialog);
-			}
-			return this._oDialog;
-      	},
-		  
-      	onOpenDialog : function () {
-        	this._getDialog().open();
-      	},
-		  
-		onCloseDialog : function () {
-			this._getDialog().close();
+		onOpenDialog : function () {
+			this.getOwnerComponent().helloDialog.open(this.getView());
 		}
 	});
 });
